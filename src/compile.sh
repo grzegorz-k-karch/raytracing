ca09a18660ee83359748d5f8bbeaf01c6530d3ce
Grzegorz Karol Karch <grzegorz.karch@gmail.com>
2020-01-04 13:47:35 +0100

    created CMakeLists.txt

--- src/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-nvcc -gencode arch=compute_75,code=sm_75 --device-c main.cu gkk_cuda_utils.cu gkk_color.cu gkk_geometry.cu gkk_object.cu gkk_material.cu gkk_random.cu gkk_vec.cu
-nvcc main.o gkk_cuda_utils.o gkk_color.o gkk_geometry.o gkk_object.o gkk_material.o gkk_random.o gkk_vec.o -gencode arch=compute_75,code=sm_75 -o a.out
